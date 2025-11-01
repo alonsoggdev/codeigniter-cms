b0795a5788979618c61f381e4e57af966957e2a7
alonsoggdev <alonso.garcia.esne@gmail.com>
2025-11-01 23:08:15 +0100

    removed views

--- src/Views/dashboard.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-<head><meta charset="utf-8"><title>Admin Dashboard</title></head>
-<body>
-    <h1>Panel Admin</h1>
-    <p>Bienvenido <?= esc(session('adminUser')['name'] ?? session('adminUser')['username']) ?></p>
-    <a href="<?= base_url('admin/logout') ?>">Cerrar sesión</a>
-</body>
-</html>
